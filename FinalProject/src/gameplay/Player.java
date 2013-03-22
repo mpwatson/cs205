daa667d06a7458fc190ec27488a8c9dd99bf9fb5
Samantha <samanthaleighkern@gmail.com>
2013-03-21 21:58:46 -0300

    Update Player.java

--- FinalProject/src/gameplay/Player.java
+++ FinalProject/src/gameplay/Player.java
@@ -9,12 +9,34 @@ public class Player {
 	/**
 	 * TODO: Add all variables the player needs here
 	 */
-	private Hand hand;
+	private String name;
+	private Hand playerHand;
 	
 	public Player(){
 		/**
 		 * TODO: Initialize the player
 		 */
+		 playerHand = new Hand();
+		
+	}
+	
+	/**
+	 * store player name
+	 * @param n
+	 *
+	 *	 -S.K.
+	 **/
+	public void setName(String n){
+		 name = n;
+	}
+	
+	/**
+	 * return the name of the player
+	 *
+	 *	 -S.K.
+	 **/
+	public String getName(){
+		return name;
 	}
 	
 	/**
@@ -22,10 +44,46 @@ public class Player {
 	 * @param cardDealt
 	 */
 	public void addCard(Card cardDealt){
-		hand.addCard(cardDealt);
+		playerHand.addCard(cardDealt);
 	}
 	
-	/**
-	 * TODO: Create all methods required for this class
-	 */
+ 	 /**
+	 * display the players hand
+	 *
+	 *	 -S.K.
+	 **/
+	 public void printHand(){
+	 	playerHand.printHand();
+	 }
+	 
+	 /**
+	 * clear the players hand
+	 *
+	 *	 -S.K.
+	 **/
+	 public void clearHand(){
+	 	playerHand.clearHand();
+	 }
+	 
+	 /**
+	 * removes card at specified index in hand
+	 * @param removeCard
+	 *
+	 *	 -S.K.
+	 **/
+	 public void removeCard(int removeCard){
+	 	playerHand.removeCard(removeCard);
+	 }
+	 
+	 
+	 /**
+	 * return card at specified index if found in hand
+	 * @param index
+	 *
+	 *	 -S.K.
+	 **/
+	 public Card getCard(int index){
+		Card c = playerHand.getCard(index);
+		return c;
+	}
 }
